b18a848c7241084ee513d604a71246ec8599d6ca
Martin Bassi <martin-bassi@hotmail.com>
2016-12-21 16:24:42 -0300

    Primera versión con un Hub

---
+++ SendNotifications/NotificationHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading;
+
+namespace SendNotifications
+{
+    public class NotificationHub : Hub
+    {
+        public void Hello(string nombreUsuario, string tipoMensaje, string mensaje)
+        {
+            Clients.All.mostrarNotificacion(nombreUsuario + mensaje, tipoMensaje);
+            /*
+            for (int i = 0; i < 4; i++)
+            {
+                Clients.All.mostrarNotificacion(nombre + mensaje, tipoMensaje);
+                Thread.Sleep(10000);
+            }*/
+        }
+    }
+}
